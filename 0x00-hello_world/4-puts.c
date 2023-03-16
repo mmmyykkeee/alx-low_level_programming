6c41940b604a523becb8b9f2798939d6d0b0a791
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-16 12:07:51 +0300

    C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,13 @@
 /**
  * main - entry point
  * description - prints 'Programming is like building multilingual puzzle
- * return is always at (0)
+ * Return is always at (0)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+/*
+* returns zero
+*/
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - entry point
+ *  printf - prints text in the std output
+ *  return always at  zero
+*/
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
