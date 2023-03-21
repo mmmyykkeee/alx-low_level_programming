5180f25162cb9c1f31081adb603f05710b98c092
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-21 16:07:17 +0300

    prints jack bauer time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,10 +14,10 @@ void jack_bauer(void)
 	for (hour = 0; hour < 24; hour++)
 	{
 	for (minute = 0; minute < 60; minute++)
-        {
+	{
 	_putchar((hour / 10) + '0');
-       	_putchar((hour % 10) + '0');
-       	_putchar(':');
+	_putchar((hour % 10) + '0');
+	_putchar(':');
 	_putchar((minute / 10) + '0');
 	_putchar((minute % 10) + '0');
 	_putchar('\n');
