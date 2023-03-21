f5e5c6374e4b5737e24d0e8c5e6e560c589ae166
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-21 13:43:38 +0300

    updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/* putchar function prototype */
-int _putchar(char c);
-
-/* print_alphabet function prototype */
-void print_alphabet(void);
-
-#endif /* MAIN_H */
-
+#ifndef main_h
+#define main_h
+extern int _putchar(char c);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int);
+extern int print_last_digit(int);
+extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+extern void print_times_table(int n);
+#endif
