011ea9d0a8cb246b069142cd11ade50456d6a7bc
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-21 16:18:28 +0300

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code
+ *
+ * @a: integer
+ *
+ * @b: integer
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
