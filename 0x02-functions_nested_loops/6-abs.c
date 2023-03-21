8e619e48c85b732aef713bf0aa9819d6de1b97f3
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-21 15:32:00 +0300

    checks the absolute of value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - the prototype function
+ *
+ * Return: gives 0 or 1 of the value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
