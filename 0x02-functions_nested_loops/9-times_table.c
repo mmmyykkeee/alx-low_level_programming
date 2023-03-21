81f8f2a85b091129677e6badef5a2df6164d0a93
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-21 16:14:28 +0300

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - breif description
+ *
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+	for (column = 0; column <= 9; column++)
+	{
+	product = row * column;
+	if (column == 0)
+	{
+	_putchar(product + '0');
+	}
+	else if (product <= 9)
+	{
+	_putchar(' ');
+	_putchar(product + '0');
+	}
+	else
+	{
+	_putchar((product / 10) + '0');
+	_putchar((product % 10) + '0');
+	}
+
+	if (column < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+
+	_putchar('\n');
+	}
+}
+
