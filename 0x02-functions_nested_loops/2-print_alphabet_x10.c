586abfab7a35d750775329344803a4d1e2feb4dc
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-21 13:17:58 +0300

    print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+	char j;
+	for (i = 0; i <= 10; i++);
+	{
+	for (j = 'a'; j <= 'z'; j++);
+	}
+	printf("%s\n", j)
+}
