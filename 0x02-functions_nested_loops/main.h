4748e5fcd9cca7694b403bd7a5fe6d8e9c8abc89
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-21 13:04:11 +0300

    main.h file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* putchar function prototype */
+int _putchar(char c);
+
+/* print_alphabet function prototype */
+void print_alphabet(void);
+
+#endif /* MAIN_H */
+
