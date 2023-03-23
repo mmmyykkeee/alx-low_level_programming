fb655ed5ac1455172329681ba8bd82495985d228
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-23 15:13:33 +0300

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,13 +14,13 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
-		return;
+	_putchar('\n');
+	return;
 	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
-		_putchar('\n');
+	for (j = 0; j < size; j++)
+	_putchar('#');
+	_putchar('\n');
 	}
 }
