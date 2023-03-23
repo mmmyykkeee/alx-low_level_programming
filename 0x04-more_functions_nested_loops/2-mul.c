520e799960fe6c2fa55336981b0ce27e5a19c8dc
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-23 12:55:25 +0300

    multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ *
+ * @a: input value
+ * @b: input value
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
