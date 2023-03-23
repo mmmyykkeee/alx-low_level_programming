b0936c7ac35d58d44e2ea9d2193e8d42d2a57e7b
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-23 12:49:46 +0300

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,6 +2,7 @@
 
 /**
  * _isdigit - checks if c is a digit or otherwise
+ *
  * @c: is the character
  * Return: Always 0
  */
@@ -10,6 +11,6 @@ int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
-	else 
+	else
 		return (0);
 }
