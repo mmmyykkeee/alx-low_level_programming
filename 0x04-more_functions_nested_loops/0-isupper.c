18c0601c6f20ba92e9b6ed92675c95639d8fff8e
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-23 12:33:14 +0300

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,6 +2,7 @@
 
 /**
  * _isupper - is the function of the program
+ *
  * @c: is the value of the program
  * Return: Always 0
  */
@@ -9,7 +10,7 @@
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
