d0cf0e16c01d998802bb4f9f85a4217752d6c597
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-27 15:03:56 +0300

    prints half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,19 +7,18 @@
  */
 void puts_half(char *str)
 {
-    int len = 0, i, n;
+	int len = 0, i, n;
 
     while (str[len] != '\0')
-        len++;
+	    len++;
 
     if (len % 2 == 0)
-        n = len / 2;
+	    n = len / 2;
     else
-        n = (len + 1) / 2;
+	    n = (len + 1) / 2;
 
     for (i = n; i < len; i++)
-        _putchar(str[i]);
+	    _putchar(str[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
-
