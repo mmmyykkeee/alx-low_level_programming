66daa7f1286ef969475fe11be5d54357c95646fd
mmmyykkeee <0mmyykkee@gmail.com>
2023-03-29 11:23:26 +0300

    Function that replaces letters with numbers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - is the value to compare
+ * @*: is the value
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char leet[] = "aAeEoOtTlL4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+		{
+			for (j = 0; j < 10; j++)
+		{
+	if (str[i] == leet[j])
+		{
+			str[i] = leet[j + 10];
+			break;
+		}
+	}
+	}
+
+	return str;
+}
