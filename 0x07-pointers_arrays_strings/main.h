397fe02a472d836e279c5736e0840e9d9a46ad52
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-04-03 12:00:42 +0300

    Fills the memory wwith a constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* function prototypes */
+char *_memset(char *s, char b, unsigned int n);
+unsigned int _strlen(char *s);
+void _puts(char *s);
+int _putchar(char c);
+
+/* macro definitions */
+#define UNUSED(x) (void)(x)
+
+#endif /* MAIN_H */
