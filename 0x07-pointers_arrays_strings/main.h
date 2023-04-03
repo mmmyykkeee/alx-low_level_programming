c14077eb09f0657a60fd938fabf763ab0caf1c8a
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-04-03 12:23:23 +0300

    a Function that gets the length of a string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ void _puts(char *s);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 /* macro definitions */
 #define UNUSED(x) (void)(x)
