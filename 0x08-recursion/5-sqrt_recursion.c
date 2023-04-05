114add01344eb68ae8331694c93f6b9f2450f18e
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-04-05 11:58:18 +0300

    fixed errors

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,41 @@
 #include "main.h"
 
 /**
-* _sqrt_recursion - returns the natural square root of a number
-* @n: number to be used
-*
-* Return: the square root of n
-*/
-
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the square root of n, or -1 if n does not have a natural square root
+ */
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+		return -1;
 	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt(0, n));
+		return n;
+	return _sqrt_helper(n, 1, n);
 }
 
 /**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
+ * _sqrt_helper - recursively find the square root of n
+ * @n: the number to find the square root of
+ * @min: the minimum possible square root of n
+ * @max: the maximum possible square root of n
  *
- * Return: the square root of n
+ * Return: the square root of n, or -1 if n does not have a natural square root
  */
-
-int _sqrt(int n, int x)
+int _sqrt_helper(int n, int min, int max)
 {
-	if (n > x / 2)
-		return (-1);
-	else if (n * n == x)
-		return (n);
-	return (_sqrt(n + 1, x));
+	int mid;
+
+	if (max < min)
+		return -1;
+
+	mid = (min + max) / 2;
+	if (mid * mid == n)
+		return mid;
+	else if (mid * mid > n)
+		return _sqrt_helper(n, min, mid - 1);
+	else
+		return _sqrt_helper(n, mid + 1, max);
 }
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt_helper(int n, int min, int max)
+int _sqrt_helper(int n, int min, int max);
 int is_prime_number(int n);
 
 #endif /* MAIN_H */
