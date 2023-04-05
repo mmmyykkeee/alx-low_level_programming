0f7104f95e20232fd303196d80b75bd6dfd041c5
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-04-05 12:00:43 +0300

    fixed errors

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,10 +9,10 @@
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	if (n == 0 || n == 1)
-		return n;
-	return _sqrt_helper(n, 1, n);
+		return (n);
+	return (_sqrt_helper(n, 1, n));
 }
 
 /**
@@ -28,14 +28,14 @@ int _sqrt_helper(int n, int min, int max)
 	int mid;
 
 	if (max < min)
-		return -1;
+		return (-1);
 
 	mid = (min + max) / 2;
 	if (mid * mid == n)
-		return mid;
+		return (mid);
 	else if (mid * mid > n)
-		return _sqrt_helper(n, min, mid - 1);
+		return (_sqrt_helper(n, min, mid - 1));
 	else
-		return _sqrt_helper(n, mid + 1, max);
+		return (_sqrt_helper(n, mid + 1, max));
 }
 
