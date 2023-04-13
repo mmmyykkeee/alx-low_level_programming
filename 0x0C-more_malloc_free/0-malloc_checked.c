30af3e473da33bcf6d5ad6b54246148b05887f7b
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-04-13 12:44:14 +0300

    task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,16 +6,16 @@
  * @b: the number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
- * If malloc fails, causes normal process termination with a status value of 98
+ * If malloc fails, causes normal process termination
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
+	void *ptr = malloc(b);
 
 	if (ptr == NULL) {
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(98);
 	}
 
-	return ptr;
+	return (ptr);
 }
