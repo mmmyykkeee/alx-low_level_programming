8f12259013b33335ab0d07450213c7bb7ef15481
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-04-17 17:29:19 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,39 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stddef.h>
-
 /**
- * struct dog - a dog's profile
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - a simple dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
  *
- * Description: Longer description
+ * Description: a simple structure representing a dog
  */
 typedef struct dog
 {
     char *name;
     float age;
     char *owner;
-    void (*init_dog)(struct dog *, char *, float, char *);
 } dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+/**
+ * init_dog - initialize a dog structure
+ * @d: pointer to the dog structure to initialize
+ * @name: name to give the dog
+ * @age: age to give the dog
+ * @owner: owner to give the dog
+ *
+ * Description: initialize a dog structure with a given name, age, and owner
+ */
+void init_dog(dog_t *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - print a dog structure
+ * @d: pointer to the dog structure to print
+ *
+ * Description: print a dog structure in a human-readable format
+ */
+void print_dog(dog_t *d);
 
 #endif /* DOG_H */
 
