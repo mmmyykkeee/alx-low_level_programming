443027c047a3bc9e54b5acbc7a59b9ee6e7aa9a1
Myke Maina <113527162+mmmyykkeee@users.noreply.github.com>
2023-04-17 17:52:11 +0300

    function that frees dogs.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -35,6 +35,7 @@ void init_dog(dog_t *d, char *name, float age, char *owner);
  */
 void print_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
 
