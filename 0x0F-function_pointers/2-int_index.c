313eee0eb4057f1ebd1180cdfedc9112ab4fc74d
mmmyykkeee <0mmyykkee@gmail.com>
2023-04-19 17:09:52 +0300

    Fixed betty

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,16 +10,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
+	int i;
 
-    if (array == NULL || cmp == NULL || size <= 0)
-        return (-1);
+	if (array == NULL || cmp == NULL || size <= 0)
+	return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]))
-            return (i);
-    }
+	for (i = 0; i < size; i++)
+	{
+	if (cmp(array[i]))
+		return (i);
+	}
 
-    return (-1);
+	return (-1);
 }
