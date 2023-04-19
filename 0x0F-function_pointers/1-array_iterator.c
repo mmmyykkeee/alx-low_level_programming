c6930e6e6c287f4d52a35f14a3c36a5fb0932949
mmmyykkeee <0mmyykkee@gmail.com>
2023-04-19 17:07:26 +0300

    Fixed betty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        action(array[i]);
-    }
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
